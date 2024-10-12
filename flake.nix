5c566ae498f00f7ec37ab63f143de571fe8d18d3
HeartBlin <heartblin@gmail.com>
2024-10-12 18:51:14 +0000

    feat: Checks && direnv

--- flake.nix
+++ flake.nix
@@ -5,9 +5,48 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
 
-      imports = [ inputs.pkgs-by-name-for-flake-parts.flakeModule ];
+      imports = [
+        inputs.pkgs-by-name-for-flake-parts.flakeModule
+        inputs.pre-commit-hooks.flakeModule
+      ];
 
-      perSystem = _: { pkgsDirectory = ./wallpapers; };
+      perSystem = { config, pkgs, ... }: {
+        # Packages
+        pkgsDirectory = ./wallpapers;
+
+        # Checks
+        pre-commit = {
+          settings = {
+            excludes = [ "flake.lock" ];
+            hooks = {
+              deadnix = {
+                enable = true;
+                verbose = true;
+                fail_fast = true;
+              };
+
+              nixfmt-classic = {
+                enable = true;
+                verbose = true;
+                fail_fast = true;
+              };
+
+              statix = {
+                enable = true;
+                verbose = true;
+                fail_fast = true;
+              };
+            };
+          };
+        };
+
+        # DevShell
+        devShells.default = pkgs.mkShellNoCC {
+          name = "Nix";
+          buildInputs = config.pre-commit.settings.enabledPackages;
+          shellHook = config.pre-commit.installationScript;
+        };
+      };
     };
 
   inputs = {
@@ -18,6 +57,12 @@
 
     pkgs-by-name-for-flake-parts.url =
       "github:drupol/pkgs-by-name-for-flake-parts";
+
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+    pre-commit-hooks.inputs = {
+      nixpkgs.follows = "nixpkgs";
+      nixpkgs-stable.follows = "nixpkgs";
+    };
 
     systems.url = "github:nix-systems/default";
   };
