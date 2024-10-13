e4baf0ce40b87475853bdf8218cec775ef2b92e9
HeartBlin <heartblin@gmail.com>
2024-10-13 11:09:07 +0000

    feat: Added matugen to devShell

--- parts/devShell.nix
+++ parts/devShell.nix
@@ -8,7 +8,8 @@
     in {
       devShells.default = mkShellNoCC {
         name = "Nix";
-        buildInputs = config.pre-commit.settings.enabledPackages;
+        buildInputs = config.pre-commit.settings.enabledPackages
+          ++ [ pkgs.matugen ];
         shellHook = config.pre-commit.installationScript;
       };
     };
